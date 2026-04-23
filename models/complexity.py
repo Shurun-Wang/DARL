047cec0c99c06583169e90aaa48f7ae7b9fad71e
Shurun-Wang <128786237+Shurun-Wang@users.noreply.github.com>
2026-04-23 10:40:27 +0800

    Update complexity.py

--- models/complexity.py
+++ models/complexity.py
@@ -1,23 +1,13 @@
 COMPLEXITY_BASELINES = {
     "adhd": {
         "DeprNet":       {"param": 0.006928, "flop": 0.00172352},
-        "InceptionTime": {"param": 1.764349, "flop": 3.536374768},
         "MBSzEEGNet":    {"param": 0.118718, "flop": 0.179880432},
         "OhCNN":         {"param": 0.000622, "flop": 0.0007108},
         "SzHNN":         {"param": 0.009814, "flop": 0.006221122},
         "STGEFormer":    {"param": 5.526912, "flop": 0.2273728},
     },
-    "gad": {
-        "DeprNet":       {"param": 0.005248, "flop": 0.00127},
-        "InceptionTime": {"param": 1.762464, "flop": 3.532604768},
-        "MBSzEEGNet":    {"param": 0.100198, "flop": 0.132580432},
-        "OhCNN":         {"param": 0.000547, "flop": 0.00056095},
-        "SzHNN":         {"param": 0.009439, "flop": 0.005476372},
-        "STGEFormer":    {"param": 4.601934, "flop": 0.166620416},
-    },
     "idd": {
         "DeprNet":       {"param": 0.005248, "flop": 0.00127},
-        "InceptionTime": {"param": 1.762464, "flop": 3.532604768},
         "MBSzEEGNet":    {"param": 0.100198, "flop": 0.132580432},
         "OhCNN":         {"param": 0.000547, "flop": 0.00056095},
         "SzHNN":         {"param": 0.009439, "flop": 0.005476372},
@@ -25,7 +15,6 @@ COMPLEXITY_BASELINES = {
     },
     "mdd": {
         "DeprNet":       {"param": 0.006928, "flop": 0.00172352},
-        "InceptionTime": {"param": 1.764349, "flop": 3.536374768},
         "MBSzEEGNet":    {"param": 0.118718, "flop": 0.179880432},
         "OhCNN":         {"param": 0.000622, "flop": 0.0007108},
         "SzHNN":         {"param": 0.009814, "flop": 0.006221122},
@@ -33,7 +22,6 @@ COMPLEXITY_BASELINES = {
     },
     "sch": {
         "DeprNet":       {"param": 0.006928, "flop": 0.00172352},
-        "InceptionTime": {"param": 1.764349, "flop": 3.536374768},
         "MBSzEEGNet":    {"param": 0.118718, "flop": 0.179880432},
         "OhCNN":         {"param": 0.000622, "flop": 0.0007108},
         "SzHNN":         {"param": 0.009814, "flop": 0.006221122},
